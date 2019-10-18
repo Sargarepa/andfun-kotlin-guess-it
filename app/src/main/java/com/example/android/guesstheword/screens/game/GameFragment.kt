cacb9ebf1a9fc08fd933b12a44f8a731b3075efe
Matija <vorkapicmatija@gmail.com>
2019-10-18 15:04:20 +0200

    Create a ViewModel for the GameFragment and add log statements when it is created and when the GameFragment initializes it

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,11 +17,15 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
@@ -53,8 +57,11 @@ class GameFragment : Fragment() {
                 false
         )
 
+
+        Log.i("GameFragment: ", "Called ViewModelProviders.of")
         // TODO (04) Create and initialize a GameViewModel, using ViewModelProviders; Add a log
         // statement
+        val gameViewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
 
         resetList()
         nextWord()
